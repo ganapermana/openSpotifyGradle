761ee0f7e9239778e2a9d6fbdfeae0d2be1884db
ganapermana <ganaariyanzah@gmail.com>
2025-12-22 01:28:27 +0700

    comment ambil location chromeDriver

--- src/main/java/Base/WebSingleton.java
+++ src/main/java/Base/WebSingleton.java
@@ -14,7 +14,7 @@ public class WebSingleton {
 
     public static WebDriver getDriver() {
         if (driver == null) {
-            System.setProperty("webdriver.chrome.driver", "/Users/ganapermana/Desktop/chromedriver"); // Sesuaikan path-nya //only for github
+//            System.setProperty("webdriver.chrome.driver", "/Users/ganapermana/Desktop/chromedriver"); // Sesuaikan path-nya //only for github
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--headless=new"); // Mode tanpa tampilan (WAJIB untuk Cloud)
             options.addArguments("--no-sandbox");

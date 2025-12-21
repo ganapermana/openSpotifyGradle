8add8aeaecd1eb0a1733c9609a6a9469a6b3bb25
ganapermana <ganaariyanzah@gmail.com>
2025-12-22 01:19:30 +0700

    add remote-allow-origins on websingelton

--- src/main/java/Base/WebSingleton.java
+++ src/main/java/Base/WebSingleton.java
@@ -19,6 +19,7 @@ public class WebSingleton {
             options.addArguments("--headless=new"); // Mode tanpa tampilan (WAJIB untuk Cloud)
             options.addArguments("--no-sandbox");
             options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--remote-allow-origins=*");
             options.addArguments("--window-size=1920,1080");
             driver = new ChromeDriver();
         }

1f2fe666ba160b823a42e87dca75ef736bd11955
ganapermana <ganaariyanzah@gmail.com>
2026-02-16 23:20:52 +0700

    add @normalScenario

--- src/test/java/TestRunner/CucumberRunner.java
+++ src/test/java/TestRunner/CucumberRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/Features", // Path ke file .feature
         glue = {"StepDefs"}, // Path ke step definitions //
-        tags = "@Scenario1",
+        tags = "@NormalScenario",
         plugin = {"pretty", "html:target/cucumber-reports.html"}, // Plugin untuk laporan
         monochrome = true // Untuk output yang lebih mudah dibaca di console
 )
